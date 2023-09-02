5a1384700f3487b89cd413eff41236343bb2c1a9
Azerty29242 <jung.jules@pm.me>
2023-09-02 22:41:36 +0200

    try to add image selector

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="container mx-auto p-4">
         <h1 style="font-size: min(10vmin, 72px)">Radios</h1>
+        <img v-for="radio in radios" :src="radio.logo" :alt="radio.name + ' logo'">
     </div>
     <Player class="fixed bottom-0"></Player>
 </template>
@@ -12,6 +13,11 @@ import radios from "./radios.js";
 export default {
     components: {
         Player: Player
+    },
+    data() {
+        return {
+            radios: radios
+        }
     }
 }
 </script>
--- src/radios.js
+++ src/radios.js
@@ -1,31 +1,37 @@
+import rtlLogo from "./assets/rtl.png"
+
 function uuid() {
     return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
 }
 
+let disableCors = function (url) {
+    return "https://cors-anywhere.herokuapp.com/" + url
+}
+
 let rtlInfos = {
     name: "RTL",
-    logo: "./../assets/rtl.png",
-    streamInfos: async function () {
-        let response = await fetch("https://www.rtl.fr/ws/live/live")
-        let data = await response.json()
-        return {
-            url: data.audio.path,
-            type: "application/vnd.apple.mpegurl"
-        }
-    },
-    programInfos: async function () {
-        let response = await fetch("https://www.rtl.fr/ws/live/live")
-        let data = await response.json()
-        return {
-            title: data.title,
-            author: data.hosts,
-            description: data.summary,
-            cover: data.cover2x,
-
-        }
-    }
+    logo: rtlLogo,
+    // streamInfos: async function () {
+    //     let response = await fetch(disableCors("https://www.rtl.fr/ws/live/live"))
+    //     let data = await response.json()
+    //     return {
+    //         url: data.audio.path,
+    //         type: "application/vnd.apple.mpegurl"
+    //     }
+    // },
+    // programInfos: async function () {
+    //     let response = await fetch(disableCors("https://www.rtl.fr/ws/live/live"))
+    //     let data = await response.json()
+    //     return {
+    //         title: data.title,
+    //         author: data.hosts,
+    //         description: data.summary,
+    //         cover: data.cover2x,
+    //
+    //     }
+    // }
 }
 
 class Radio {
@@ -33,20 +39,14 @@ class Radio {
         this.id = uuid()
         this.name = name
         this.logo = logo
-        this.stream = Object.assign({}, streamInfosCallback())
-        this.program = Object.assign({}, programInfosCallback())
-        this.refreshProgram = async function () {
-            this.program = await programInfosCallback()
-        }
-        console.log(this)
-    }
-
-    get logoSrc() {
-        return this.logo
-    }
-
-    get streamUrl() {
-        return this.stream
+        // this.stream = Object.assign({}, streamInfosCallback())
+        // this.program = Object.assign({}, programInfosCallback())
+        // this.refreshStream = function () {
+        //     this.program = programInfosCallback()
+        // }
+        // this.refreshProgram = function () {
+        //     this.program = programInfosCallback()
+        // }
     }
 
     get programInfos() {
@@ -54,8 +54,10 @@ class Radio {
     }
 }
 
+let rtl = new Radio(rtlInfos.name, rtlInfos.logo)
+
 let radios = [
-    new Radio(rtlInfos.name, rtlInfos.logo, rtlInfos.streamInfos, rtlInfos.programInfos)
+    rtl
 ]
 
 export default radios
